a41d248dfa648cee74ec53fb6feb6116538e0c67
EmeraldSkye <ebyrd6@lsu.edu>
2012-07-31 13:40:38 -0500

    Initial Commit

---
+++ HelloApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloApp
+//
+//  Created by Emerald Byrd on 7/31/12.
+//  Copyright (c) 2012 Skye Byrd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HelloApp/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  HelloApp
+//
+//  Created by Emerald Byrd on 7/31/12.
+//  Copyright (c) 2012 Skye Byrd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+@end
